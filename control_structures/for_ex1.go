cab7b84e3888eefc6e484aa957fe4d9676e7c663
nanofaroque <faroque.ae@gmail.com>
2018-08-31 10:57:20 -0500

    adding another for loop for learning

--- control_structures/for_ex1.go
+++ control_structures/for_ex1.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main(){
-	i:=1;
+	i:=1 // Initialize the variable
 	for i<10{
-		fmt.Println(i);
-		i = i+1;
+		fmt.Println(i)
+		i = i+1
 	}
+
+
+
+	sum:=0;
+	for j:=0;j<100;j++{
+		sum+=j
+		fmt.Println("I am printing the value of sum: "+strconv.Itoa(sum))
+	}
+
+
 }

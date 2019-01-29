a6c2903f6b5b2e2cd5a3785a7cd416e8b6c3223b
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-29 10:49:50 -0600

    counting chars in golang

---
+++ data_structures/count-chars.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare
+	var m map[string]int
+	// initialize map
+	m = make(map[string]int)
+	// input string
+	var s string = "hello world"
+	// iterate and count
+	for i := 0; i < len(s); i++ {
+		fmt.Print(string(s[i]) + "->")
+		if (m[string(s[i])] == 0) {
+			m[string(s[i])] = 1;
+		} else {
+			m[string(s[i])] = m[string(s[i])] + 1;
+		}
+	}
+	//print map
+	fmt.Println(m)
+}

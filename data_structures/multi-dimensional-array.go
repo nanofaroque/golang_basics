d5b85bc4379d7f0a7492a0cc17853325b894b86e
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-01-29 22:30:41 -0600

    multi dimensional array

---
+++ data_structures/multi-dimensional-array.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/**
+	declare and initialize an multi-dimensional array
+	*/
+	var in [][]int = [][] int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	// printing each element
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			fmt.Print(in[i][j])
+		}
+	}
+	fmt.Print(in);
+}

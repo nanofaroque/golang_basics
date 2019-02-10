262acc68a5a4b027787531f847b5d526a62feaf3
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-09 18:57:09 -0600

    palindrom number

---
+++ data_structures/palindrom_numer.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	isPalindrome(121)
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	var rem = x
+	var new = 0
+	for rem/10 != 0 {
+		var t = rem % 10
+		rem = rem / 10
+		fmt.Println("%d,%d",rem,t)
+		new = new*10 + t
+	}
+	new=new*10+rem
+
+	if new!=x{
+		return false
+	}
+
+	return true
+}

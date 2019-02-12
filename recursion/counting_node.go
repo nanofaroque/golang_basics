32a5620fcd036fcdc1991856f5ffefee8afae650
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-02-11 19:49:41 -0600

    counting nodes of linked list in golang

---
+++ recursion/counting_node.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	val int
+	next *Node
+}
+
+func main()  {
+	root:=&Node{1,nil}
+	root.next=&Node{2,nil}
+	root.next.next=&Node{3,nil}
+	res:=print_node(root)
+	fmt.Print(res)
+}
+
+func print_node(node *Node) int{
+	if node==nil{
+		return 0
+	}
+	fmt.Print(node.val)
+	return node.val+print_node(node.next)
+}
